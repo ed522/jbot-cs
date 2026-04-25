a3275e59e1dbf9ed1e205daf7946b800c77f7b4f
ed522 <147557022+ed522@users.noreply.github.com>
2026-04-24 21:22:53 -0400

    Add unit tests - currently for parser and lexer

---
+++ Jbot.Test/LexerTests.cs
@@ -0,0 +1,274 @@
+﻿using Jbot.Nametable.Parse;
+
+namespace Jbot.Test;
+
+public class LexerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        Console.WriteLine("Tests start");
+    }
+
+    private string Space(string str)
+    {
+        return "   " + str + "  ";
+    }
+
+    #region Lexer tests - individual symbols
+    [Test]
+    public void TestLexer_Identifier()
+    {
+        string input = Space("Some_Identifier");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(1));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[0].value, Is.EqualTo("Some_Identifier"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_Number()
+    {
+        string input = Space("12345");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(1));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.NUMBER));
+            Assert.That(output[0].value, Is.EqualTo("12345"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_DescendingIdentifier()
+    {
+        string input = Space("some.descending.identifier");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(1));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.DESCENDING_IDENTIFIER));
+            Assert.That(output[0].value, Is.EqualTo("some.descending.identifier"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_DeclVersion()
+    {
+        string input = Space("version");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(1));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.DECL_VERSION));
+            Assert.That(output[0].value, Is.EqualTo("version"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_DeclAttrib()
+    {
+        string input = Space("attrib");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(1));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.DECL_ATTRIB));
+            Assert.That(output[0].value, Is.EqualTo("attrib"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_DeclObject()
+    {
+        string input = Space("object");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(1));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.DECL_OBJECT));
+            Assert.That(output[0].value, Is.EqualTo("object"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_DeclBind()
+    {
+        string input = this.Space("bind");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(1));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.DECL_BIND));
+            Assert.That(output[0].value, Is.EqualTo("bind"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_DeclField()
+    {
+        string input = this.Space("field");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(1));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.DECL_FIELD));
+            Assert.That(output[0].value, Is.EqualTo("field"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_DeclAllows()
+    {
+        string input = this.Space("allows");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(1));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.DECL_ALLOWS));
+            Assert.That(output[0].value, Is.EqualTo("allows"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_DeclType()
+    {
+        string input = this.Space("type");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(1));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.DECL_TYPE));
+            Assert.That(output[0].value, Is.EqualTo("type"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_BlockStart()
+    {
+        string input = this.Space("id1{id2");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(3));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[0].value, Is.EqualTo("id1"));
+            Assert.That(output[1].type, Is.EqualTo(SymbolType.BLOCK_START));
+            Assert.That(output[1].value, Is.EqualTo("{"));
+            Assert.That(output[2].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[2].value, Is.EqualTo("id2"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_BlockEnd()
+    {
+        string input = this.Space("id1}id2");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(3));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[0].value, Is.EqualTo("id1"));
+            Assert.That(output[1].type, Is.EqualTo(SymbolType.BLOCK_END));
+            Assert.That(output[1].value, Is.EqualTo("}"));
+            Assert.That(output[2].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[2].value, Is.EqualTo("id2"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_StatementEnd()
+    {
+        string input = this.Space("id1;id2");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(3));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[0].value, Is.EqualTo("id1"));
+            Assert.That(output[1].type, Is.EqualTo(SymbolType.STATEMENT_END));
+            Assert.That(output[1].value, Is.EqualTo(";"));
+            Assert.That(output[2].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[2].value, Is.EqualTo("id2"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_DeclId()
+    {
+        string input = this.Space("id1#id2");
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(3));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[0].value, Is.EqualTo("id1"));
+            Assert.That(output[1].type, Is.EqualTo(SymbolType.DECL_ID));
+            Assert.That(output[1].value, Is.EqualTo("#"));
+            Assert.That(output[2].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[2].value, Is.EqualTo("id2"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_TypeSeparator()
+    {
+        const string input = "id1,id2";
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(3));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[0].value, Is.EqualTo("id1"));
+            Assert.That(output[1].type, Is.EqualTo(SymbolType.TYPE_SEPARATOR));
+            Assert.That(output[1].value, Is.EqualTo(","));
+            Assert.That(output[2].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[2].value, Is.EqualTo("id2"));
+        });
+    }
+
+    [Test]
+    public void TestLexer_DeclFieldBind()
+    {
+        const string input = "id1$id2";
+        Symbol[] output = Lexer.Parse(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(output, Is.Not.EqualTo(null));
+            Assert.That(output, Has.Length.EqualTo(3));
+            Assert.That(output[0].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[0].value, Is.EqualTo("id1"));
+            Assert.That(output[1].type, Is.EqualTo(SymbolType.DECL_FIELD_BIND));
+            Assert.That(output[1].value, Is.EqualTo("$"));
+            Assert.That(output[2].type, Is.EqualTo(SymbolType.IDENTIFIER));
+            Assert.That(output[2].value, Is.EqualTo("id2"));
+        });
+    }
+    #endregion
+
+    [Test]
+    public void TestLexer_InvalidSymbol()
+    {
+        string input = this.Space("valid symbols *****");
+        Assert.Throws<SyntaxException>(() => Lexer.Parse(input));
+    }
+}
---
+++ Jbot.Test/ParserTests.cs
@@ -0,0 +1,317 @@
+﻿using static Jbot.Nametable.Parse.NodeType;
+
+using Jbot.Nametable.Parse;
+
+namespace Jbot.Test;
+
+public class ParserTests
+{
+    /// <summary>
+    /// Run a parameterized parser test. The input string is lexed and parsed,
+    /// and the output of the parser (the children of the root node) is
+    /// compared to the expected output.
+    /// </summary>
+    /// <param name="input">the input to be parsed</param>
+    /// <param name="expected">the expected output of the parser, in terms of the root's children</param>
+    private static void TestValid(string input, Node[] expected)
+    {
+        Symbol[] lexerOut = Lexer.Parse(input);
+        Node output = Parser.Parse(lexerOut);
+
+        Assert.That(output.Children, Has.Count.EqualTo(expected.Length));
+        Assert.That(output.Children, Is.EqualTo(expected));
+    }
+    private static void TestInvalid(string input)
+    {
+        Symbol[] lexerOut = Lexer.Parse(input);
+        Assert.Throws<SyntaxException>(() => Parser.Parse(lexerOut));
+    }
+    
+    // all test cases must be invocations since Node instantiations aren't compile-time constants
+    #region Valid parser scenarios
+    [Test]
+    public void TestParserOutput_Empty() => TestValid("", []);
+    [Test]
+    public void TestParserOutput_Version() => TestValid(
+        "version 10000;", [
+            new Node(VERSION, "10000"),
+        ]);
+    [Test]
+    public void TestParserOutput_DocumentAttrib() => TestValid(
+        "attrib attr1 attr2;", [
+        new Node(TOP_LEVEL_ATTRIBUTE_SET, [
+            new Node(TOP_LEVEL_ATTRIBUTE, "attr1"),
+            new Node(TOP_LEVEL_ATTRIBUTE, "attr2"),
+        ]),
+    ]);
+
+    [Test]
+    public void TestParserOutput_ObjectBasic() => TestValid(
+        "object ObjName {}", [
+            new Node(OBJECT, [
+                new Node(OBJECT_NAME, "ObjName"),
+            ]),
+        ]);
+
+    [Test]
+    public void TestParserOutput_ObjectId() => TestValid(
+        "object ObjName # 123 {}", [
+            new Node(OBJECT, [
+                new Node(OBJECT_NAME, "ObjName"),
+                new Node(OBJECT_ID, "123"),
+            ]),
+        ]);
+
+    [Test]
+    public void TestParserOutput_ObjectAttributes() => TestValid(
+        "object ObjName { attrib attr1 attr2; }", [
+            new Node(OBJECT, [
+                new Node(OBJECT_NAME, "ObjName"),
+                new Node(OBJECT_ATTRIBUTE_SET, [
+                    new Node(OBJECT_ATTRIBUTE, "attr1"),
+                    new Node(OBJECT_ATTRIBUTE, "attr2"),
+                ]),
+            ]),
+        ]);
+
+    [Test]
+    public void TestParserOutput_ObjectBind() => TestValid(
+        "object ObjName { bind target.path.first target.path.second; }", [
+            new Node(OBJECT, [
+                new Node(OBJECT_NAME, "ObjName"),
+                new Node(OBJECT_BIND, [
+                    new Node(OBJECT_BIND_TARGET, "target.path.first"),
+                    new Node(OBJECT_BIND_TARGET, "target.path.second"),
+                ]),
+            ]),
+        ]);
+
+    [Test]
+    public void TestParserOutput_FieldShortForm_Basic() => TestValid(
+        "object ObjName { field field1; }", [
+            new Node(OBJECT, [
+                new Node(OBJECT_NAME, "ObjName"),
+                new Node(FIELD, [
+                    new Node(FIELD_ID, "field1"),
+                ]),
+            ]),
+        ]);
+
+    [Test]
+    public void TestParserOutput_FieldShortForm_Type() => TestValid(
+        "object ObjName { field field1 type TYPEA,TYPEB; }", [
+            new Node(OBJECT, [
+                new Node(OBJECT_NAME, "ObjName"),
+                new Node(FIELD, [
+                    new Node(FIELD_ID, "field1"),
+                    new Node(FIELD_TYPE_SET, [
+                        new Node(FIELD_TYPE, "TYPEA"),
+                        new Node(FIELD_TYPE, "TYPEB"),
+                    ]),
+                ]),
+            ]),
+        ]);
+
+    [Test]
+    public void TestParserOutput_FieldShortForm_Allows() => TestValid(
+        "object ObjName { field field1 allows Obj1, Obj2; }", [
+            new Node(OBJECT, [
+                new Node(OBJECT_NAME, "ObjName"),
+                new Node(FIELD, [
+                    new Node(FIELD_ID, "field1"),
+                    new Node(FIELD_ALLOWS, [
+                        new Node(FIELD_ALLOWED_OBJECT, "Obj1"),
+                        new Node(FIELD_ALLOWED_OBJECT, "Obj2"),
+                    ]),
+                ]),
+            ]),
+        ]);
+
+    [Test]
+    public void TestParserOutput_FieldShortForm_Bind() => TestValid(
+        "object ObjName { field field1 $bind.path1 $bind.path2; }", [
+            new Node(OBJECT, [
+                new Node(OBJECT_NAME, "ObjName"),
+                new Node(FIELD, [
+                    new Node(FIELD_ID, "field1"),
+                    new Node(FIELD_BIND, [
+                        new Node(FIELD_BIND_TARGET, "bind.path1"),
+                        new Node(FIELD_BIND_TARGET, "bind.path2"),
+                    ]),
+                ]),
+            ]),
+        ]);
+
+    [Test]
+    public void TestParserOutput_FieldShortForm_Attribute() => TestValid(
+        "object ObjName { field field1 attr1 attr2; }", [
+            new Node(OBJECT, [
+                new Node(OBJECT_NAME, "ObjName"),
+                new Node(FIELD, [
+                    new Node(FIELD_ID, "field1"),
+                    new Node(FIELD_ATTRIBUTE_SET, [
+                        new Node(FIELD_ATTRIBUTE, "attr1"),
+                        new Node(FIELD_ATTRIBUTE, "attr2"),
+                    ]),
+                ]),
+            ]),
+        ]);
+
+    [Test]
+    public void TestParserOutput_FieldShortForm_Combined() => TestValid(
+        "object ObjName { field field1 attr1 type type1 allows Obj1 $bind.path attr2; }", [
+            new Node(OBJECT, [
+                new Node(OBJECT_NAME, "ObjName"),
+                new Node(FIELD, [
+                    new Node(FIELD_ID, "field1"),
+                    new Node(FIELD_TYPE_SET, [
+                        new Node(FIELD_TYPE, "type1"),
+                    ]),
+                    new Node(FIELD_ALLOWS, [
+                        new Node(FIELD_ALLOWED_OBJECT, "Obj1"),
+                    ]),
+                    new Node(FIELD_BIND, [
+                        new Node(FIELD_BIND_TARGET, "bind.path"),
+                    ]),
+                    new Node(FIELD_ATTRIBUTE_SET, [
+                        new Node(FIELD_ATTRIBUTE, "attr1"),
+                        new Node(FIELD_ATTRIBUTE, "attr2"),
+                    ]),
+                ]),
+            ])
+        ]);
+
+    [Test]
+    public void TestParserOutput_FieldLongForm() => TestValid(
+        """
+        object ObjName { 
+            field field1 {
+                type TYPEA,TYPEB;
+                allows Obj1,Obj2;
+                bind $bind.path1 $bind.path2;
+                attrib attr1 attr2;
+            }
+        }
+        """, [
+            new Node(OBJECT, [
+                new Node(OBJECT_NAME, "ObjName"),
+                new Node(FIELD, [
+                    new Node(FIELD_ID, "field1"),
+                    new Node(FIELD_TYPE_SET, [
+                        new Node(FIELD_TYPE, "TYPEA"),
+                        new Node(FIELD_TYPE, "TYPEB"),
+                    ]),
+                    new Node(FIELD_ALLOWS, [
+                        new Node(FIELD_ALLOWED_OBJECT, "Obj1"),
+                        new Node(FIELD_ALLOWED_OBJECT, "Obj2"),
+                    ]),
+                    new Node(FIELD_BIND, [
+                        new Node(FIELD_BIND_TARGET, "bind.path1"),
+                        new Node(FIELD_BIND_TARGET, "bind.path2"),
+                    ]),
+                    new Node(FIELD_ATTRIBUTE_SET, [
+                        new Node(FIELD_ATTRIBUTE, "attr1"),
+                        new Node(FIELD_ATTRIBUTE, "attr2"),
+                    ]),
+                ]),
+            ]),
+        ]);
+
+    #endregion
+    #region Invalid parser scenarios
+    
+    // NOTE: every test that looks for a missing token should have an extra token at the end to
+    // properly trigger the logic, instead of triggering a throw in the parser's advancement
+    // logic (if there's no tokens during an accept it just throws an "unexpected file end" error)
+    #region Top-level
+    [Test] public void TestParserValidation_TopLevel_UnexpectedSymbol() => 
+        TestInvalid("123");
+    
+    [Test] public void TestParserValidation_DocumentAttrib_MissingAttributes() => 
+        TestInvalid("attrib;");
+    [Test] public void TestParserValidation_DocumentAttrib_MissingSemicolon() => 
+        TestInvalid("attrib attr1 version");
+    
+    [Test] public void TestParserValidation_Version_MissingNumber() => 
+        TestInvalid("version;");
+    [Test] public void TestParserValidation_Version_MissingSemicolon() => 
+        TestInvalid("version 1 attrib");
+    #endregion
+    
+    #region Objects
+    [Test] public void TestParserValidation_Object_MissingStartBrace() => 
+        TestInvalid("object O }");
+    [Test] public void TestParserValidation_Object_MissingEndBrace() => 
+        TestInvalid("object O { object O2 { }");
+    [Test] public void TestParserValidation_Object_UnexpectedSymbol() => 
+        TestInvalid("object O { version }");
+    
+    [Test] public void TestParserValidation_Object_MissingName() => 
+        TestInvalid("object #1 { }");
+    [Test] public void TestParserValidation_ObjectId_MissingNumber() => 
+        TestInvalid("object O # { }");
+    
+    [Test] public void TestParserValidation_ObjectAttrib_MissingSemicolon() => 
+        TestInvalid("object O { attrib a }");
+    [Test] public void TestParserValidation_ObjectBind_MissingTarget() => 
+        TestInvalid("object O { bind ; }");
+    [Test] public void TestParserValidation_ObjectBind_MissingSemicolon() => 
+        TestInvalid("object O { bind a.b }");
+    #endregion
+    
+    #region Fields (short-form)
+    [Test] public void TestParserValidation_Field_MissingName() => 
+        TestInvalid("object O { field ; }");
+    [Test] public void TestParserValidation_Field_UnexpectedSymbol() => 
+        TestInvalid("object O { field f version }");
+    [Test] public void TestParserValidation_FieldId_MissingNumber() => 
+        TestInvalid("object O { field f #; }");
+    
+    [Test] public void TestParserValidation_FieldType_MissingType() => 
+        TestInvalid("object O { field f type ; }");
+    [Test] public void TestParserValidation_FieldType_TrailingComma() => 
+        TestInvalid("object O { field f type t, ; }");
+    
+    [Test] public void TestParserValidation_FieldAllows_MissingObject() => 
+        TestInvalid("object O { field f allows ; }");
+    [Test] public void TestParserValidation_FieldAllows_TrailingComma() => 
+        TestInvalid("object O { field f allows t1,t2, ; }");
+    
+    [Test] public void TestParserValidation_FieldBind_MissingTarget() => 
+        TestInvalid("object O { field f $ ; }");
+    #endregion
+    
+    #region Fields (long-form)
+    [Test] public void TestParserValidation_LongField_UnexpectedSymbol() => 
+        TestInvalid("object O { field f { version } }");
+    [Test] public void TestParserValidation_LongField_MissingBrace() => 
+        TestInvalid("object O { field f { type t;");
+    
+    [Test] public void TestParserValidation_LongFieldBind_MissingDollar() => 
+        TestInvalid("object O { field f { bind target; } }");
+    [Test] public void TestParserValidation_LongFieldBind_MissingTarget() => 
+        TestInvalid("object O { field f { bind $; } }");
+    [Test] public void TestParserValidation_LongFieldBind_MissingSemicolon() => 
+        TestInvalid("object O { field f { bind $b } }");
+    
+    [Test] public void TestParserValidation_LongFieldType_MissingSemicolon() => 
+        TestInvalid("object O { field f { type t } }");
+    [Test] public void TestParserValidation_LongFieldType_MissingTypeName() => 
+        TestInvalid("object O { field f { type ; } }");
+    [Test] public void TestParserValidation_LongFieldType_TrailingComma() => 
+        TestInvalid("object O { field f { type t, ; } }");
+    
+    [Test] public void TestParserValidation_LongFieldAllows_MissingSemicolon() => 
+        TestInvalid("object O { field f { allows o } }");
+    [Test] public void TestParserValidation_LongFieldAllows_MissingObject() => 
+        TestInvalid("object O { field f { allows ; } }");
+    
+    [Test] public void TestParserValidation_LongFieldAttrib_MissingSemicolon() => 
+        TestInvalid("object O { field f { attrib a } }");
+    [Test] public void TestParserValidation_LongFieldAttrib_MissingAttribute() => 
+        TestInvalid("object O { field f { attrib ; } }");
+    #endregion
+    
+    #endregion
+    
+}
