c8197363c6fe971f81bfde491159b53be17b2095
ed522 <147557022+ed522@users.noreply.github.com>
2026-04-24 21:22:53 -0400

    Give proper operators, GetHashCode, ToString to Node, use correct types, and add a FIELD_TYPE_SET node type

--- Jbot/Nametable/Parse/Node.cs
+++ Jbot/Nametable/Parse/Node.cs
@@ -1,3 +1,5 @@
+using System.Collections.Immutable;
+
 namespace Jbot.Nametable.Parse;
 
 internal enum NodeType
@@ -8,27 +10,52 @@ internal enum NodeType
     OBJECT, OBJECT_BIND, OBJECT_ATTRIBUTE_SET,
     OBJECT_BIND_TARGET, OBJECT_ATTRIBUTE, OBJECT_ID, OBJECT_NAME, 
     
-    FIELD, FIELD_BIND, FIELD_ALLOWS, FIELD_ATTRIBUTE_SET, FIELD_TYPE,
+    FIELD, FIELD_BIND, FIELD_ALLOWS, FIELD_ATTRIBUTE_SET, FIELD_TYPE_SET, FIELD_TYPE,
     FIELD_ATTRIBUTE, FIELD_ID, FIELD_BIND_TARGET, FIELD_ALLOWED_OBJECT, FIELD_NAME,
 }
 
-internal class Node(NodeType type, string value, Node[] children)
+internal class Node(NodeType type, string value, ICollection<Node> children)
 {
     public Node(NodeType type) : this(type, "", []) {}
     public Node(NodeType type, string value) : this(type, value, []) {}
-    public Node(NodeType type, Node[] children) : this(type, "", children) {}
+    public Node(NodeType type, ICollection<Node> children) : this(type, "", children) {}
     public NodeType Type { get; init; } = type;
     public string Value { get; init; } = value;
-    public List<Node> Children { get; init; } = [.. children];
+    public ImmutableHashSet<Node> Children { get; init; } = [..children];
 
+    public static bool operator ==(Node? left, Node? right)
+    {
+        if (ReferenceEquals(left, right)) return true;
+        if (left is null || right is null) return false;
+        return left.Equals(right);
+    }
+    public static bool operator !=(Node? left, Node? right) => !(left == right);
+    
     public override bool Equals(object? other)
     {
         return other is Node n
             && n.Type == this.Type
             && n.Value == this.Value
-            && n.Children.SequenceEqual(this.Children);
+            && n.Children.SetEquals(this.Children);
     }
 
-    public override int GetHashCode() => 
-        HashCode.Combine(this.Type, this.Value, this.Children);
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        hash.Add(this.Type);
+        hash.Add(this.Value);
+
+        int childrenHash = 0;
+        foreach (var child in this.Children)
+            childrenHash ^= child.GetHashCode(); // XOR is order-independent
+
+        hash.Add(childrenHash);
+        return hash.ToHashCode();
+    }
+
+    public override string ToString()
+    {
+        return $"type={this.Type}, value={this.Value}, children=[{string.Join(", ", this.Children)}]";
+    }
+    
 }
